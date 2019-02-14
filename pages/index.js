17f3f55965ad2a2fd6ea04718e9375a5beae0d98
Parker Sutera <parker.sutera@apothecom.com>
2019-02-14 15:51:30 -0500

    global style

--- pages/index.js
+++ pages/index.js
@@ -10,28 +10,28 @@ function getPosts () {
 }
 
 const PostLink = ({ post }) => (
-  <li>
-    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
-      <a>{post.title}</a>
-    </Link>
-    <style jsx>{`
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
-
-      a {
-        text-decoration: none;
-        color: blue;
-        font-family: "Arial";
-      }
-
-      a:hover {
-        opacity: 0.6;
-      }
-    `}</style>
-  </li>
-)
+    <li>
+      <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+        <a>{post.title}</a>
+      </Link>
+      <style jsx>{`
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+  
+        a {
+          text-decoration: none;
+          color: blue;
+          font-family: "Arial";
+        }
+  
+        a:hover {
+          opacity: 0.6;
+        }
+      `}</style>
+    </li>
+  )
 
 export default () => (
   <Layout>
